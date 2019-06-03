3ba7d0ca1e90e1348c3e281835ad1c3339774849
agarheim <agarheim@gmail.com>
2019-06-03 20:06:18 +0300

    first stap php world

---
+++ rach.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skillup_student
+ * Date: 03.06.19
+ * Time: 19:37
+ */
+$parametrs= ['operand1','operand2','operation'];
+foreach ($parametrs as $param) {
+
+    if (empty($_REQUEST[$param])) { die ("not whrite $param");}}
+$o1=floatval($_REQUEST['operand1']);
+$o2=floatval($_REQUEST['operand2']);
+$d=$_REQUEST['operation'];
+
+switch ($d)
+{
+    case '+': $result=$o1+$o2;
+        break;
+    case '-': $result=$o1-$o2;
+        break;
+    case '*': $result=$o1*$o2;
+        break;
+    case '/': $result=$o1+$o2;
+        break;
+    default: die('not enough operator');
+}
+echo $o1.$d.$o2.'='.$result;
