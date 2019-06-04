0d91ccff33e2e9176cc401113b5e5c6a2005af00
Roman L <agarheim@gf.com>
2019-06-04 15:31:10 +0300

    refactor array

--- index.php
+++ index.php
@@ -1,49 +1,41 @@
 <?php
-echo '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>-------------------------------1-----------------------------<br/>';
+ $write='';
+ $write2='';
+ $write3='';
+
+
 $a =[1,2,3,4,5];
 $count=0;
 foreach ($a as $value) {
     $value = $value * 2;
     $count+=$value;
-    echo "sqrt : $value <br /> count : $count <br />";
+    $write.= "sqrt : $value <br /> count : $count <br />";
 }
 unset ($count, $value);
-echo '--------------------------------2----------------------------<br/>';
+
 $name=['Коля'=>200, 'Вася'=> 300, 'Петя'=>400];
 foreach ($name as $key => $value) {
     $value = $value * 2;
 
-    echo "$key - зарплата $value долларов. <br /> ";
+    $write2.= "$key - зарплата $value долларов. <br /> ";
 }
 unset ($key, $value);
-?>
-<p>-------------------------------3-------------------</p>
-<?php
+
 $week=['Понедельник'=>1, 'Вторник'=>2, 'Среда'=>3, 'Четверг'=>4, 'Пятница'=>5];
-$day= date(N);
+$day= date('N');
 foreach ($week as $key => $value) {
-    if ($value == $day) echo "<em> $key </em><br />";  else echo "$key <br />";
+    if ($value == $day) $write3.= "<em> $key </em><br />";  else $write3.="$key <br />";
 
 }
 unset ($key, $value);
-?>
 
-<p>------------------------------4-------------------</p>
-<?php
 $arr = ['green'=>'зеленый', 'red'=>'красный', 'blue'=>'голубой'];
 foreach ($arr as $key => $value) {
     $en[]=$key;
     $ru[]=$value;
 }
-print_r($en);
-echo '<br />';
-print_r($ru);
-echo '
-</body>
-</html>';
+$write4= implode ('|',$en);
+$write5= implode ('|',$ru);
+require ('templ.php');
+
+
