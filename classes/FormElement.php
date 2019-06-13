33a9bba9367db65064478888de851978c0b7b8f8
Roman L <agarheim@gf.com>
2019-06-13 09:06:39 +0300

    worked

--- classes/FormElement.php
+++ classes/FormElement.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: skillup_student
- * Date: 10.06.19
- * Time: 19:30
- */
 
 abstract class FormElement
 {
@@ -12,56 +6,50 @@ abstract class FormElement
      * @var string
      */
     private $name;
-    /** @var string */
-    private $label;
-    /**
-     * @var
-     */
- protected $value;
-    /**
-     * @var bool
-     */
- protected $required='false';
     /**
      * @var string
      */
- protected $error='';
-public function __construct(string $name, string $label, bool $required=false)
-{
-    $this->name=$name;
-    $this->label=$label;
-    $this->required=$required;
-}
-
+    private $label;
+    /**
+     * @var string
+     */
+    protected $value;
+    /**
+     * @var bool
+     */
+    protected $required = false;
+    /**
+     * @var string
+     */
+    protected $error = '';
+    public function __construct(string $name, string $label, bool $required = false)
+    {
+        $this->name = $name;
+        $this->label = $label;
+        $this->required = $required;
+    }
     public function getName(): string
     {
         return $this->name;
     }
-
     public function getLabel(): string
     {
         return $this->label;
     }
-
-    public function getValue() : string
+    public function getValue(): string
     {
-       return $this->value;
+        return $this->value;
     }
-
-    public function setValue(string $value) :void
+    public function setValue(string $value): void
     {
         $this->value = $value;
-        if (empty($value)&& $this->required)
-       {$this->error='Whats wrong';}
-
+        if (empty($value) && $this->required) {
+            $this->error = 'Поле не должно быть пустым';
+        }
     }
-
-
     public function getError(): string
     {
         return $this->error;
     }
-
-abstract public function render() :string;
-
+    abstract public function render(): string;
 }
