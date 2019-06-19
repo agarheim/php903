23b09416f2211c6ec1d2c8ad96d2a52e4caaad91
agarheim <agarheim@gmail.com>
2019-06-19 20:46:14 +0300

    dell all

---
+++ classes/Form.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skillup_student
+ * Date: 10.06.19
+ * Time: 19:15
+ */
+
+class Form
+{ //это свойство класса
+    /**
+     * @var FormElement[]
+     */
+    private $elements;
+    /**
+     * @var
+     */
+    private $method;
+    /**
+     * @var boolean
+     */
+    private $isSubmitted=false;
+    public function __construct(string  $method='post')
+    {
+        $this->method=strtolower($method);
+    }
+
+    public function add (FormElement $element)
+    {
+        $this->elements[$element->getName()]=$element;
+
+    }
+
+    public function getElements()
+    {
+        return $this->elements;
+    }
+    public function render()
+    {
+        $html=sprintf('<form method="%s" action="">',$this->method);
+        foreach ($this->elements as $element)
+        {
+            $html.=$element->render().'<br>';
+
+        }
+        $html.='</form>';
+        return $html;
+    }
+    public function handleRequest()
+    {
+        $data= $this->method=='post'? $_POST: $_GET;
+        foreach ($this->elements as $element) {
+            if (isset($data[$element->getName()]))
+            {  $this->isSubmitted='true';
+                $element->setValue($data[$element->getName()]);
+            }
+        }
+        foreach ($this->elements as $element)
+        {
+            if ($element->getError())
+            {
+                $this->isSubmitted='false';
+                break;
+            }
+        }
+    }
+
+    public function setValue($name)
+    {
+        return $this->elements[$name]->getValue();
+    }
+    public function isSubmitted():bool
+    {
+        return $this->isSubmitted;
+    }
+}
---
+++ classes/FormElement.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skillup_student
+ * Date: 10.06.19
+ * Time: 19:30
+ */
+
+abstract class FormElement
+{
+    /**
+     * @var string
+     */
+    private $name;
+    /** @var string */
+    private $label;
+    /**
+     * @var string
+     */
+    protected $value;
+    /**
+     * @var bool
+     */
+    protected $required='false';
+    /**
+     * @var string
+     */
+    protected $error='';
+    public function __construct(string $name, string $label, bool $required=false)
+    {
+        $this->name=$name;
+        $this->label=$label;
+        $this->required=$required;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function getValue() : string
+    {
+        $this->value;
+    }
+
+    public function setValue(string $value) :void
+    {
+        $this->value = $value;
+        if (empty($value)&& $this->required)
+        {$this->error='Whats wrong';}
+
+    }
+
+
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+    abstract public function render() :string;
+
+}
